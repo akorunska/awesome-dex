fc0e623623bcfe0b89b0f53d4c538a6811ff3c9d
Anna Korunska <akorunska@gmail.com>
2019-08-13 17:02:47 +0300

    mocked order data as if from ontology contract

--- frontend/src/pages/orderbook.js
+++ frontend/src/pages/orderbook.js
@@ -2,6 +2,27 @@ import React, { Component } from 'react';
 import { PageHeader } from 'antd';
 
 class Orderbook extends Component {
+    state = {
+        orders: [
+            {
+                coinToBuy: 'ETH',
+                amountToBuy: 0.5,
+                coinToSell: 'ONT',
+                amountToSell: 200,
+                status: 'opened',
+                initiatorEthAddr: '',
+                initiatorOntAddr: 'AcKEYhZw8Gdux8R9jc3vhDYbCQCXRH44HD',
+                buyerEthAddr: '',
+                buyerOntAddr: '',
+                hashlock: 'lock',
+                initiatorRefundTimelock: 0,
+                buyerRefundTimelock: 0,
+                initiatorClaimTimelock: 0,
+                secret: '',
+            }
+        ]
+    }
+    
     render() {
         return (
             <>
