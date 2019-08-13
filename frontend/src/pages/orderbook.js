296ea161f54325c5b47148787281790ff3d70726
Anna Korunska <akorunska@gmail.com>
2019-08-13 17:31:56 +0300

    displaying mock data on orderbook page

--- frontend/src/pages/orderbook.js
+++ frontend/src/pages/orderbook.js
@@ -1,32 +1,70 @@
 import React, { Component } from 'react';
-import { PageHeader } from 'antd';
+import { PageHeader, Button, Table } from 'antd';
+
+const orderData = [
+    {
+        coinToBuy: 'ETH',
+        amountToBuy: 0.5,
+        coinToSell: 'ONT',
+        amountToSell: 200,
+        status: 'opened',
+        initiatorEthAddr: '',
+        initiatorOntAddr: 'AcKEYhZw8Gdux8R9jc3vhDYbCQCXRH44HD',
+        buyerEthAddr: '',
+        buyerOntAddr: '',
+        hashlock: 'lock',
+        initiatorRefundTimelock: 0,
+        buyerRefundTimelock: 0,
+        initiatorClaimTimelock: 0,
+        secret: '',
+    }
+];
+
+const orderBookColumns = [
+    {
+        title: "For sell",
+        dataIndex: "",
+        key: "sell",
+        render: record => (record ? record.coinToSell + " " + record.amountToSell  : "n/a"),
+    },
+    {
+        title: "For buy",
+        dataIndex: "",
+        key: "buy",
+        render: record => (record ? record.coinToBuy + " " + record.amountToBuy  : "n/a"),
+    },
+    {
+        title: "Actions",
+        render: res => (
+            <div>
+                <Button
+                    type="warning"
+                    icon="forward"
+                    onClick={() => {
+                        // send tx to smart contract
+                    }}
+                >
+                        Respond to order
+                </Button>
+            </div>
+        ),
+    },
+];
 
 class Orderbook extends Component {
     state = {
-        orders: [
-            {
-                coinToBuy: 'ETH',
-                amountToBuy: 0.5,
-                coinToSell: 'ONT',
-                amountToSell: 200,
-                status: 'opened',
-                initiatorEthAddr: '',
-                initiatorOntAddr: 'AcKEYhZw8Gdux8R9jc3vhDYbCQCXRH44HD',
-                buyerEthAddr: '',
-                buyerOntAddr: '',
-                hashlock: 'lock',
-                initiatorRefundTimelock: 0,
-                buyerRefundTimelock: 0,
-                initiatorClaimTimelock: 0,
-                secret: '',
-            }
-        ]
+        orderData: orderData,
     }
-    
+
     render() {
         return (
             <>
-                <PageHeader> DEX Orderbook </PageHeader>
+                <PageHeader title="DEX Orderbook" subTitle="Here you can view orders and respond to them"/>
+                <Table
+					columns={orderBookColumns}
+                    dataSource={orderData}
+                    style={{marginTop: '1em'}}
+				/>
             </>
           );
     }
