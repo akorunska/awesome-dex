c1922da410234d8c6be9cf4d9639647d29f57208
Anna Korunska <akorunska@gmail.com>
2020-03-04 18:31:08 +0200

    added info to homepage

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,12 +1,36 @@
-import React, { Component } from 'react';
-import { PageHeader } from 'antd';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { PageHeader, Card, Descriptions } from "antd";
 
 class Home extends Component {
-    render() {
-        return (
-            <PageHeader title="Home"/>
-          );
-    }
+  render() {
+    const { user } = this.props;
+
+    return (
+      <>
+        <PageHeader title="Awesome DEX Home" />
+        <Card style={{ marginTop: 20 }}>
+          <Descriptions
+            title={`You are currently logged in as ${user.displayName}`}
+          >
+            <Descriptions.Item label="Ontology address">
+              {user.ontAddress}
+            </Descriptions.Item>
+            <Descriptions.Item label="Ontology address (hex)">
+              {user.ontAddressByteArray}
+            </Descriptions.Item>
+            <Descriptions.Item label="Ethereum address">
+              {user.ethAddress}
+            </Descriptions.Item>
+          </Descriptions>
+        </Card>
+      </>
+    );
+  }
 }
 
-export default Home;
+export default connect(state => {
+  return {
+    user: state.user
+  };
+})(Home);
